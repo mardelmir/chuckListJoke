d05d57133e544cf691636430ad6463c0a19a1984
María Delgado <mariad1996@gmail.com>
2023-11-25 16:58:40 +0100

    css

--- js/script.js
+++ js/script.js
@@ -1,49 +1,89 @@
-const btnFetchList = document.getElementById('fetchJoke');
-const fetchJoke = document.getElementById('jokeList');
+const btnFetchJoke = document.getElementById('fetchJoke');
+const jokeList = document.getElementById('jokeList');
 
 
 window.addEventListener('load', () => {
     if (!localStorage.data) {
-        fetchJoke.innerHTML=''
+        jokeList.innerHTML = ''
     } else {
         let guardados = JSON.parse(localStorage.data)
         guardados.forEach(elemento => {
-            mostrar(elemento)
+            mostrar(elemento.valor)
         })
     }
 })
 
-btnFetchList.addEventListener('click', () => {
+btnFetchJoke.addEventListener('click', () => {
     fetch('https://api.chucknorris.io/jokes/random')
         .then(response => {
             if (!response.ok) {
-                throw new error('no se pudo obtener')
+                throw new Error('No se pudo obtener el contenido')
             }
             return response.json()
         })
         .then(data => {
-            mostrar (data.value)
+            mostrar(data.value)
             guardar(data.value)
-    })
+        })
+        .catch(error => {
+            console.error('Error al mostrar el contenido')
+            jokeList.innerText = 'No se puede mostrar el contenido'
+        })
 })
 
-const mostrar = (joke) => {
+const mostrar = (chiste) => {
     const plantilla = `
-        <li>
-            <p>${joke}</p>
-            <button id ="borrar" class="eliminar">Eliminar</button>
+        <li class="chiste">
+            <p>${chiste}</p>
+            <button class="eliminar">Eliminar</button>
         </li>`
-    fetchJoke.innerHTML += plantilla
+    jokeList.innerHTML += plantilla
 }
 
 const guardar = (broma) => {
-    if(!localStorage.data) {
-        let chiste = JSON.stringify([broma])
+    const item = {
+        //id: `${id}`,
+        valor: broma,
+    }
+    if (!localStorage.data) {
+        let chiste = JSON.stringify([item])
+        console.log(chiste)
         localStorage.setItem('data', chiste)
     } else {
         let antiguo = JSON.parse(localStorage.data)
-        let actual = [...antiguo, broma]
+        let actual = [...antiguo, item]
         localStorage.data = JSON.stringify(actual)
     }
 }
 
+
+
+const btnEliminarUno = document.querySelectorAll('.eliminar')
+btnEliminarUno.forEach(btn => {
+    let li = btn.parentNode
+    console.log(li);
+})
+
+const btnEliminarTodo = document.createElement('button')
+btnEliminarTodo.textContent = 'Eliminar todo'
+btnEliminarTodo.setAttribute('id', 'clear')
+jokeList.insertAdjacentElement('afterend', btnEliminarTodo)
+
+btnEliminarTodo.addEventListener('click', () => {
+    localStorage.clear()
+    jokeList.innerHTML = ''
+})
+
+
+
+
+// const guardar = (broma) => {
+//     if (!localStorage.data) {
+//         let chiste = JSON.stringify([broma])
+//         localStorage.setItem('data', chiste)
+//     } else {
+//         let antiguo = JSON.parse(localStorage.data)
+//         let actual = [...antiguo, broma]
+//         localStorage.data = JSON.stringify(actual)
+//     }
+// }
